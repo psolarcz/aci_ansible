db32aeffaefd1e9f98a5473618a21a1a45ddaeb7
psolarcz <psolarcz@cisco.com>
2019-07-19 08:19:56 +0200

    commit

--- roles/tenant/tasks/start.sh
+++ roles/tenant/tasks/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh -i ~/.ssh/git 192.168.206.237 'cd Documents/aci && git pull && ansible-playbook network_deploy.yaml  -e state=present'
+ssh -i ~/.ssh/git 192.168.206.237 'cd Documents/aci && git pull && ansible-playbook network_deploy.yaml  -e state=absent'
